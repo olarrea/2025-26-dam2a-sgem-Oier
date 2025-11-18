d94830c4e12384d26e935e6333d51d250dac7484
olarrea <olarreairi2@educacion.navarra.es>
2025-11-18 09:21:35 +0100

    Ejercicios Dia 18

---
+++ 2025_11_18/Ejercicio_02.py
@@ -0,0 +1,5 @@
+import random
+
+dado =  random.randint(1, 6)
+
+print(f"Ha salido el número {dado}")
---
+++ 2025_11_18/Ejercicio_03.py
@@ -0,0 +1,3 @@
+minutos = int(input("Introduce una cantidad de minutos:"))
+salida = minutos * 60
+print(f"Entrada: {minutos} - Salida: {salida}")
